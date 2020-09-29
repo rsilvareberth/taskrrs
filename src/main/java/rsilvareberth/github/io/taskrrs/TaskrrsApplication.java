7792df8ad4c54b3b062a5ccd83f0ada98ef459a7
Reberth <rsilvareberth@gmail.com>
2020-09-28 21:21:01 -0300

    Spring boot initial

---
+++ src/main/java/rsilvareberth/github/io/taskrrs/TaskrrsApplication.java
@@ -0,0 +1,13 @@
+package rsilvareberth.github.io.taskrrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskrrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskrrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rsilvareberth/github/io/taskrrs/TaskrrsApplicationTests.java
@@ -0,0 +1,13 @@
+package rsilvareberth.github.io.taskrrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskrrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
